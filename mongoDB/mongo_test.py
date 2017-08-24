1523d53a2f5e5d6fecae4ed39f36dfd4badd4bef
linxiaodan <linxiaodan>
2017-08-24 21:02:56 +0800

    8-24

---
+++ mongoDB/mongo_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created on 17-8-24
+# Author: LXD
+
+import pymongo
+
+conn = pymongo.MongoClient(host='localhost', port=)
---
+++ supervisor/scripts/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created on 17-8-24
+# Author: LXD
+
+while 1:
+    pass
